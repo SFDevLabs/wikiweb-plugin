6365afa70ea27d8c628cf7c24445eb9a307ffc34
Eoin McMillan <eoinmcmillan@gmail.com>
2016-11-06 18:44:50 -0800

    pulling url from the page

--- app/containers/App.js
+++ app/containers/App.js
@@ -16,18 +16,48 @@ import style from './App.css';
 )
 export default class App extends Component {
 
+  constructor() {
+    super()
+    this.state = {tabURL: ''}
+  }
+
   static propTypes = {
     todos: PropTypes.array.isRequired,
     actions: PropTypes.object.isRequired
   };
 
+  componentWillMount() {
+    const that = this;
+    chrome.tabs.query({
+        active: true,
+        currentWindow: true
+      }, function(tabs) {
+          that.setState({tabURL: tabs[0].url.split('?')[0]})
+      });
+  }
+
   render() {
     const { todos, actions } = this.props;
+    const { tabURL } = this.state;
 
     return (
-      <div className={style.normal}>
-        <Header addTodo={actions.addTodo} />
-        <MainSection todos={todos} actions={actions} />
+      <div>
+        <div style={{display:'block', marginTop: '10px'}}>
+
+          <div style={{float: 'left', marginLeft: '10px', marginRight: '10px', height: '100px'}}>
+            <div style={{height: '48px', width: '48px', backgroundColor: 'brown', borderRadius: '4px'}}>
+            </div>
+          </div>
+
+          <div style={{float: 'left', height: '100px' }}>
+            <div>
+              <p style={{margin: 0, padding: 0}}>Title</p>
+              <p style={{margin: 0, padding: 0}}>Description</p>
+              <p style={{margin: 0, padding: 0}}>{tabURL}</p>
+            </div>
+          </div>
+
+        </div>
       </div>
     );
   }
