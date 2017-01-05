4d7308243b0c07e9c391a2016cc9310bcf6f4779
Eoin McMillan <eoinmcmillan@gmail.com>
2017-01-05 16:13:52 +1100

    misc change

--- app/actions/index.js
+++ app/actions/index.js
@@ -88,16 +88,16 @@ function setExtensionButon(entityCount){
   });
   // Set the path for the icon
   const path = entityCount > 0 ?
-  {
-    "16": "img/icon-16-connected.png",
-    "48": "img/icon-48-connected.png",
-    "128": "img/icon-128-connected.png",
-  }:
-  {
-    "16": "img/icon-16-not-connected.png",
-    "48": "img/icon-48-not-connected.png",
-    "128": "img/icon-128-not-connected.png",
-  };
+    {
+      "16": "img/icon-16-connected.png",
+      "48": "img/icon-48-connected.png",
+      "128": "img/icon-128-connected.png",
+    } :
+    {
+      "16": "img/icon-16-not-connected.png",
+      "48": "img/icon-48-not-connected.png",
+      "128": "img/icon-128-not-connected.png",
+    };
 
   chrome.browserAction.setIcon({
     path
