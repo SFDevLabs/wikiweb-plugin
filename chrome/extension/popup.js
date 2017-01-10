84a6081ee8060418c6325d9f2cf3b091190daed7
Jeff Jenkins <jenkinsjeffrey@gmail.com>
2017-01-09 16:59:48 -0800

    Sass

--- chrome/extension/popup.js
+++ chrome/extension/popup.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from '../../app';
-import '../assets/style/app.css';
+import '../assets/style/app.scss';
 
 
 ReactDOM.render(
--- webpack/dev.config.js
+++ webpack/dev.config.js
@@ -57,6 +57,10 @@ const baseDevConfig = () => ({
         'css?modules&sourceMap&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]',
         'postcss'
       ]
+    },
+    {
+      test: /\.scss$/,
+      loaders: ['style-loader', 'css-loader', 'sass-loader']
     }]
   }
 });
