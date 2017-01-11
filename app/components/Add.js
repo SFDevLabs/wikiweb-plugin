0aebd8dfa99674cf2e57eee483b6855066e1d154
Eoin McMillan <eoinmcmillan@gmail.com>
2017-01-11 18:16:57 +1100

    good stopping point. some styling done on form. no submit functions written.

--- app/components/Add.js
+++ app/components/Add.js
@@ -23,13 +23,17 @@ class Add extends Component {
   render() {
     return (
       <div className={'addJS'} style={{ minHeight: 266, paddingTop: 4, fontFamily: 'Verdana, Geneva, sans-serif', color: '#000000', borderTop: '3px solid #70037C' }}>
-        <Link to="/" >back</Link>
-        <form>
-          <input />
-          <input />
-          <input />
-        </form>
-        <div>Add Holder <input onKeyDown={this.onKeyDown} value={this.state.val} /></div>
+        <Link to="/" style={{ textDecoration: 'none' }} ><span style={{ paddingLeft: 6, paddingRight: 6, fontSize: 24, color: '#70037C' }} >&#8592;</span></Link>
+        <div style={{ paddingLeft: '7%', paddingTop: 5, width: '100%' }} >
+          <form >
+            <input className={'formInput'} placeholder="URL..." style={{ marginBottom: 10 }} />
+            <textarea className={'formInput'} placeholder="Description..." style={{ marginBottom: 7, height: 70, lineHeight: '20px', paddingTop: 8 }} />
+            <input className={'formInput'} placeholder="Tags" style={{ marginBottom: 10 }} />
+            <div>
+              <input className={'formSubmit'} type="submit" />
+            </div>
+          </form>
+        </div>
       </div>
     );
   }
