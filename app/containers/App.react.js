7477698fc4aa6e206320d88fa8375992b7ee565d
Eoin McMillan <eoinmcmillan@gmail.com>
2017-01-23 18:03:14 +1100

    added react-ga and ga in app.react.js file (needs testing with live data)

--- app/containers/App.react.js
+++ app/containers/App.react.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import { Router, Route, hashHistory } from 'react-router';
+import ReactGA from 'react-ga';
 import Connections from './Connections.react';
 import Login from './Login.react';
 import Add from './Add.react';
+ReactGA.initialize('UA-90661699-1');
+
+function logPageView() {
+  ReactGA.set({ page: window.location.pathname });
+  ReactGA.pageview(window.location.pathname);
+}
 
 class App extends Component {
-
   render() {
     return (
-      <Router history={hashHistory}>
+      <Router history={hashHistory} onUpdate={logPageView}>
         <Route path="/" component={Connections} />
         <Route path="add" component={Add} />
         <Route path="login" component={Login} />
