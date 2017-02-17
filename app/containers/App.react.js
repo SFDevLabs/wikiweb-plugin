26eb20ad871320f477bf843680189533f5ad2e3d
Jeff Jenkins <jenkinsjeffrey@gmail.com>
2017-02-16 18:58:51 -0800

    Google analytics works.

--- app/containers/App.react.js
+++ app/containers/App.react.js
@@ -1,18 +1,18 @@
 import React, { Component, PropTypes } from 'react';
 import { Router, Route, hashHistory } from 'react-router';
 import { connect } from 'react-redux';
-import ReactGA from 'react-ga';
+//import ReactGA from 'react-ga';
 import Connections from './Connections.react';
 import Login from './Login.react';
 import Add from './Add.react';
 import { fetchSearch } from '../actions/entity';
 import { fetchProfile } from '../actions/user';
 
-ReactGA.initialize('UA-90661699-1');
-
 function logPageView() {
-  ReactGA.set({ page: window.location.pathname });
-  ReactGA.pageview(window.location.pathname);
+  if ( typeof(_gaq) !== 'undefined'){
+    _gaq.push(['_setAccount', 'UA-90661699-1']);
+    _gaq.push(['_trackPageview', location.hash]);
+  }
 }
 
 class App extends Component {
@@ -46,3 +46,18 @@ class App extends Component {
 }
 
 export default connect()(App);
+
+// Load Google Analytics
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  ga.onload = function(){
+    setTimeout(function(){
+      _gaq.push(['_setAccount', 'UA-90661699-1']);
+      _gaq.push(['_trackPageview', 'extentionOpened']);
+      logPageView();
+    }, 10)
+  }
+})();
