ddc02541f1365a831fd7e19f12e5abfdd7363b60
Jeff Jenkins <jenkinsjeffrey@gmail.com>
2017-02-21 23:02:19 -0800

    Removing dom insert.

--- chrome/scripts/inject.js
+++ chrome/scripts/inject.js
@@ -1,7 +1,3 @@
-var root = document.createElement('div');
-root.id = "wikiwebRoot";
-document.body.prepend(root);
-
 var script = document.createElement('script');
 script.type = "application/javascript";
 script.src = "https://localhost:4000/js/popup.bundle.js"; // use this for linked script
