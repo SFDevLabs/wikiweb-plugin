a166e4822dfba3e3c3f923592a5c2916b480c6c7
Jeff Jenkins <jenkinsjeffrey@gmail.com>
2017-02-22 15:42:56 -0800

    Adding background back in.

--- chrome/scripts/inject.js
+++ chrome/scripts/inject.js
@@ -1,4 +1,8 @@
-var script = document.createElement('script');
-script.type = "application/javascript";
-script.src = "//localhost:4000/js/popup.bundle.js"; // use this for linked script
-document.body.appendChild(script);
+chrome.storage.local.get(['wikiwebFooterActive'], function (res) {
+  if (res.wikiwebFooterActive === true) {
+    const script = document.createElement('script');
+    script.type = 'application/javascript';
+    script.src = '//localhost:4000/js/popup.bundle.js'; // use this for linked script
+    document.body.appendChild(script);
+  }
+});
