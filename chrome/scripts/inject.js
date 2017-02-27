cc9141699ad6b92aa91033475a404b5b683b7a1b
Eoin McMillan <eoinmcmillan@gmail.com>
2017-02-27 13:35:12 -0800

    fonts / font awesome moved into iframe project folder

--- chrome/scripts/inject.js
+++ chrome/scripts/inject.js
@@ -1,7 +1,9 @@
 iframe = document.createElement('iframe');
-iframe.src='chrome-extension://akjooaompalcndlllaolceeeaonodbhp/main.html';
+iframe.src='chrome-extension://chlabnnbbloifmgcmenphjdpakeaoijm/main.html';
 iframe.style.width = "100%";
 iframe.style.position = "fixed";
 iframe.style.left = 0;
 iframe.style.bottom = 0;
+iframe.style.border = 'none';
+iframe.style.height = 100; /* 45px height corresponds with plugin height in stylesheet */ /* TODO: set to 45px when done styling */
 document.body.append(iframe);
