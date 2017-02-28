214a0b1958c0931358bd1ee339f5bf08b2a5163c
Jeff Jenkins <jenkinsjeffrey@gmail.com>
2017-02-27 16:29:14 -0800

    Adding injected footer

--- chrome/scripts/inject.js
+++ chrome/scripts/inject.js
@@ -7,3 +7,7 @@ iframe.style.bottom = 0;
 iframe.style.border = 'none';
 iframe.style.height = 46; /* 45px height corresponds with plugin height in stylesheet. We're using 46 here to account for the borderTop/boxShadowTop */
 document.body.append(iframe);
+
+paddingFooterDiv = document.createElement('div');
+paddingFooterDiv.style.height = 45;
+document.body.append(paddingFooterDiv);
